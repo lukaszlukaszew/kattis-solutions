e51fb8fbadabdffd50653ac248def39ce3a56c16
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-06 10:26:58 +0200

    A Real Challenge, Another Brick in the Wall

---
+++ A/A Real Challenge.py
@@ -0,0 +1,5 @@
+"""A Real Challenge"""
+
+from math import sqrt
+
+print(format(sqrt(int(input())) * 4, ".6f"))
---
+++ A/Another Brick in the Wall.py
@@ -0,0 +1,19 @@
+"""Another Brick in the Wall"""
+
+h, w, n = map(int, input().split())
+x = list(map(int, input().split()))[::-1]
+
+for i in range(h):
+    row = 0
+
+    while row < w and x:
+        row += x[-1]
+        x.pop()
+
+    if row != w:
+        break
+
+if i == h - 1 and row == w:
+    print("YES")
+else:
+    print("NO")
