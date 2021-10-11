2b51a2e8f776e93b08a23154b4661df3cd771ab2
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-11 11:53:11 +0200

    Add Army Strength

---
+++ A/Army Strength (Hard).py
@@ -0,0 +1,25 @@
+"""Army Strength (Hard)"""
+
+from sys import stdin, stdout
+
+cases = int(stdin.readline())
+winners = []
+
+for i in range(cases):
+    stdin.readline()
+    armies = [int(x) for x in stdin.readline().split()]
+    godzilla = sorted([int(x) for x in stdin.readline().split()], reverse=True)
+    mechagodzilla = sorted([int(x) for x in stdin.readline().split()], reverse=True)
+
+    while len(godzilla) * len(mechagodzilla) != 0:
+        if godzilla[-1] < mechagodzilla[-1]:
+            godzilla.pop()
+        else:
+            mechagodzilla.pop()
+
+    if len(godzilla) == 0 and len(mechagodzilla) != 0:
+        stdout.write("MechaGodzilla\n")
+    elif len(godzilla) != 0 and len(mechagodzilla) == 0:
+        stdout.write("Godzilla\n")
+    else:
+        stdout.write("uncertain\n")
