43588fae6a951c8f7a3c7ec070e1326555cad1a1
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-13 08:34:08 +0200

    Beavergnaw, Beekeeper

---
+++ B/Beekeeper.py
@@ -0,0 +1,18 @@
+"""Beekeeper"""
+
+vowels = ["a", "e", "i", "o", "u", "y"]
+word_count = int(input())
+
+while word_count:
+    words = []
+    values = []
+
+    for i in range(word_count):
+        words.append(input())
+        value = 0
+        for j in vowels:
+            value += words[-1].count(j*2)
+        values.append(value)
+
+    print(words[values.index(max(values))])
+    word_count = int(input())
