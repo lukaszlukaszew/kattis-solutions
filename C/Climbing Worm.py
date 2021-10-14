e9c18b3964678ec14028a5369066aecc90314fdb
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-14 18:33:56 +0200

    Add Cold-puter Science, Climbing Worm

---
+++ C/Climbing Worm.py
@@ -0,0 +1,15 @@
+"""Climbing Worm"""
+
+crawl, fall, height = [int(x) for x in input().split()]
+distance, counter = 0, 0
+
+while distance < height:
+    distance += crawl
+    counter += 1
+
+    if distance >= height:
+        break
+    else:
+        distance -= fall
+
+print(counter)
---
+++ C/Cold-puter Science.py
@@ -0,0 +1,4 @@
+"""Cold-puter Science"""
+
+input()
+print(len(list(filter(lambda x: x < 0, map(int, input().split())))))
