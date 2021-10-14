959014db98b1150a913a5a18809c78d24b2e57b5
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-14 09:57:25 +0200

    Add Card Trick, Chanukah Challenge

---
+++ C/Card Trick.py
@@ -0,0 +1,24 @@
+"""Card Trick"""
+
+from collections import deque
+
+cases = int(input())
+
+for i in range(cases):
+    num = int(input())
+    variables = "abcdefghijklmnop"
+    cards = []
+    shuffle = deque()
+
+    for j in range(num):
+        cards.append(variables[j])
+        shuffle.append(variables[j])
+
+    for j in range(1, num + 1):
+        for k in range(j):
+            shuffle.append(shuffle.popleft())
+
+        cards[cards.index(shuffle[0])] = j
+        shuffle.popleft()
+
+    print(*cards)
---
+++ C/Chanukah Challenge.py
@@ -0,0 +1,11 @@
+"""Chanukah Challenge"""
+
+data_sets = int(input())
+
+for i in range(data_sets):
+    K, N = map(int, input().split())
+
+    if N == 1:
+        print(K, 2)
+    else:
+        print(K, int((3 + N) / 2 * N))
