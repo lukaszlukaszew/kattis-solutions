3b47950daea958d7881d5a01a5b237eea07f7254
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-14 10:11:17 +0200

    Add CD, Careful Ascent

---
+++ C/Careful Ascent.py
@@ -0,0 +1,19 @@
+"""Careful Ascent"""
+
+x, y = [int(x) for x in input().split()]
+
+shields_count = int(input())
+shields = []
+road = 0
+shilded_road = 0
+
+for i in range(shields_count):
+    shields.append(tuple(float(x) for x in input().split()))
+
+for i in shields:
+    road += (i[1] - i[0]) * i[2]
+    shilded_road += i[1] - i[0]
+
+road += y - shilded_road
+
+print(x / road)
