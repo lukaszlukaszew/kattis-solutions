f6092f31e5fa441a4d69a61b6fc3caf84af25189
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-15 10:45:02 +0200

    Add Cudoviste, Curse the Darkness

---
+++ C/Curse the Darkness.py
@@ -0,0 +1,20 @@
+"""Curse the Darkness"""
+
+cases = int(input())
+
+for i in range(cases):
+    bookX, bookY = [float(x) for x in input().split()]
+
+    candles_count = int(input())
+    result = 0
+
+    for j in range(candles_count):
+        candX, candY = [float(x) for x in input().split()]
+
+        if (bookX - candX) ** 2 + (bookY - candY) ** 2 - 8 ** 2 <= 0:
+            result = 1
+
+    if result:
+        print("light a candle")
+    else:
+        print("curse the darkness")
