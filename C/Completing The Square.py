2f1a21dbdfa6a401f1bb54b1c7dba8e59dbde2ba
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-16 10:34:42 +0200

    Add Yin and Yang Stones, Completing The Square

---
+++ C/Completing The Square.py
@@ -0,0 +1,20 @@
+"""Completing The Square"""
+
+from math import sqrt
+
+[Ax, Ay] = [int(x) for x in input().split()]
+[Bx, By] = [int(x) for x in input().split()]
+[Cx, Cy] = [int(x) for x in input().split()]
+X, Y = 0, 0
+
+if sqrt((Bx - Ax) ** 2 + (By - Ay) ** 2) == sqrt((Bx - Cx) ** 2 + (By - Cy) ** 2):
+    X = Cx + Ax - Bx
+    Y = Ay + Cy - By
+elif sqrt((Cx - Ax) ** 2 + (Cy - Ay) ** 2) == sqrt((Cx - Bx) ** 2 + (Cy - By) ** 2):
+    X = Ax + Bx - Cx
+    Y = By + Ay - Cy
+elif sqrt((Ax - Bx) ** 2 + (Ay - By) ** 2) == sqrt((Ax - Cx) ** 2 + (Ay - Cy) ** 2):
+    X = Cx + Bx - Ax
+    Y = By + Cy - Ay
+
+print(X, Y)
---
+++ Y/Yin and Yang Stones.py
@@ -0,0 +1,4 @@
+"""Yin and Yang Stones"""
+
+line = input()
+print(int(line.count("W") == line.count("B")))
