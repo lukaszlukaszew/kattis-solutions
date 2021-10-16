53284c46e1775884707259ae03c15494a09f08b4
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-16 10:26:53 +0200

    Add Birthday Memorization, N-sum

---
+++ B/Birthday Memorization.py
@@ -0,0 +1,16 @@
+"""Birthday Memorization"""
+
+cases = int(input())
+birthdays, likeness = {}, {}
+
+for i in range(cases):
+    name, like, birthday = input().split()
+
+    if likeness.get(birthday, -1) < int(like):
+        likeness[birthday] = int(like)
+        birthdays[birthday] = name
+
+print(len(birthdays))
+
+for i in sorted(birthdays.values()):
+    print(i)
---
+++ N/N-sum.py
@@ -0,0 +1,4 @@
+"""N-sum"""
+
+input()
+print(sum(map(int, input().split())))
