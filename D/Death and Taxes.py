387bfe905de62c607c55ffc6e268139993c90cdb
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-17 11:58:48 +0200

    Add Detailed Differences, Death and Taxes

---
+++ D/Death and Taxes.py
@@ -0,0 +1,26 @@
+"""Death and Taxes"""
+
+from sys import stdin
+
+p, q, s, a = 0, 0, 0, 0
+
+while True:
+    instr, *nums = stdin.readline().split()
+    nums = list(map(int, nums))
+
+    if instr == "sell":
+        s -= nums[0]
+    elif instr == "buy":
+        p = nums[0] * nums[1]
+        q = nums[0]
+        a = (p + a * s) / (q + s)
+        s += q
+    elif instr == "split":
+        s *= nums[0]
+        a /= nums[0]
+    elif instr == "merge":
+        s //= nums[0]
+        a *= nums[0]
+    elif instr == "die":
+        print(s * (nums[0] - max(0, nums[0] - a) * 0.3))
+        break
---
+++ D/Detailed Differences.py
@@ -0,0 +1,17 @@
+"""Detailed Differences"""
+
+cases = int(input())
+
+for i in range(cases):
+    upper_line = input()
+    lower_line = input()
+    result = ""
+    for j, val in enumerate(upper_line):
+        if val == lower_line[j]:
+            result += "."
+        else:
+            result += "*"
+
+    print(upper_line)
+    print(lower_line)
+    print(result, "\n")
