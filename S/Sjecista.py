0af1ea64776a9f0caa408f390b0724e6f2f890f4
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-18 08:20:38 +0200

    Add Sjecista, Skener

---
+++ S/Sjecista.py
@@ -0,0 +1,4 @@
+"""Sjecista"""
+
+N = int(input())
+print(int(N * (N - 1) * (N - 2) * (N - 3) / 24))
---
+++ S/Skener.py
@@ -0,0 +1,11 @@
+"""Skener"""
+
+r, c, y, x = map(int, input().split())
+
+for i in range(r):
+    line = input()
+    for j in range(y):
+        result_line = ""
+        for k in line:
+            result_line += k * x
+        print(result_line)
