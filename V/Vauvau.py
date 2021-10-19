33a539b9ef81da87073ba448e0f83122ff36143a
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-19 08:26:17 +0200

    Add Vauvau, Volim

---
+++ V/Vauvau.py
@@ -0,0 +1,14 @@
+"""Vauvau"""
+
+A, B, C, D = [int(x) for x in input().split()]
+heroes = [int(x) for x in input().split()]
+results = {0: "none", 1: "one", 2: "both"}
+
+for i in range(3):
+    result = 0
+    if 0 < heroes[i] % (A + B) <= A:
+        result += 1
+    if 0 < heroes[i] % (C + D) <= C:
+        result += 1
+
+    print(results[result])
