c00a410d7fa0eb1317e7197a2cc305aa318ae1a8
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-19 08:28:57 +0200

    Add Snapper Chain Easy & Hard

---
+++ S/Snapper Chain (Hard).py
@@ -0,0 +1,13 @@
+"""Snapper Chain (Hard)"""
+
+cases = int(input())
+
+for i in range(cases):
+    n, k = [int(x) for x in input().split()]
+
+    if k - (2 ** n - 1) >= 0 and (k - (2 ** n - 1)) % (2 ** n) == 0:
+        result = "ON"
+    else:
+        result = "OFF"
+
+    print("Case #" + str(i + 1) + ":", result)
