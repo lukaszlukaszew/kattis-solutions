0be61758e84150cd53dbbc463006cb8c31e7f67c
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-20 08:15:33 +0200

    Add Incognito, Inflation

---
+++ I/Inflation.py
@@ -0,0 +1,13 @@
+"""Inflation"""
+
+items = int(input())
+canisters = sorted(list(map(int, input().split())))
+result = []
+
+for i in range(items):
+    result.append(canisters[i] / (i + 1))
+
+if max(result) > 1:
+    print("impossible")
+else:
+    print(format(min(result), ".6f"))
