ef869d1294361585353e4f40b7e592dec5d753f2
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-22 09:04:13 +0200

    Add Encoded Message, Engineering English

---
+++ E/Encoded Message.py
@@ -0,0 +1,15 @@
+"""Encoded Message"""
+
+from math import sqrt
+
+cases = int(input())
+
+for i in range(cases):
+    message = input()
+    side = int(sqrt(len(message)))
+    result = ""
+    for j in range(side):
+        for k in range(side):
+            result += message[side * (k + 1) - j - 1]
+
+    print(result)
---
+++ E/Engineering English.py
@@ -0,0 +1,21 @@
+"""Engineering English"""
+
+import sys
+from collections import defaultdict
+
+duplicates = defaultdict(int)
+
+try:
+    while True:
+        line = input().split()
+
+        for i, val in enumerate(line):
+            if duplicates[val.lower()]:
+                line[i] = "."
+
+            duplicates[val.lower()] += 1
+
+        print(" ".join(line))
+
+except EOFError:
+    sys.exit()
