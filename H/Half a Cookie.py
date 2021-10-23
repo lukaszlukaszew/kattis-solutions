894595f515d26eab31e8f729531e25292da027a1
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-23 09:23:04 +0200

    Add Half a Cookie, Hangman

---
+++ H/Half a Cookie.py
@@ -0,0 +1,17 @@
+"""Half a Cookie"""
+
+import math as mt
+
+while True:
+    try:
+        r, x, y = [float(x) for x in input().split()]
+        s = mt.sqrt(x ** 2 + y ** 2)
+
+        if r > s:
+            area = r ** 2 / 2 * (mt.acos(s / r) * 2 - mt.sin(mt.acos(s / r) * 2))
+            area2 = r ** 2 * mt.pi
+            print(area2 - area, area)
+        else:
+            print("miss")
+    except EOFError:
+        break
