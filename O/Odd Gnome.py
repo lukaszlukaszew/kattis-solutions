8a784c1db214dc7c9070f03ab0bd8b52b91d67e1
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-23 09:24:06 +0200

    Add Odd Gnome, Odd Man Out

---
+++ O/Odd Gnome.py
@@ -0,0 +1,11 @@
+"""Odd Gnome"""
+
+groups = int(input())
+
+for i in range(groups):
+    gnomes = list(map(int, input().split()))
+
+    for j in range(1, len(gnomes)):
+        if gnomes[j + 1] - gnomes[j] != 1:
+            print(j + 1)
+            break
---
+++ O/Odd Man Out.py
@@ -0,0 +1,10 @@
+"""Odd Man Out"""
+
+from collections import Counter
+
+cases = int(input())
+
+for i in range(cases):
+    input()
+    guests = Counter(input().split())
+    print(f"Case #{i+1}: {list(guests.keys())[list(guests.values()).index(1)]}")
