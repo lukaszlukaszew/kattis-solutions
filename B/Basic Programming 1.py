8e9e6292e7d0b3c460a1402fbdd291ba2c876111
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-23 09:22:28 +0200

    Add Basic Programming 1 & 2

---
+++ B/Basic Programming 1.py
@@ -0,0 +1,36 @@
+"""Basic Programming 1"""
+
+from string import ascii_lowercase
+
+num, action = map(int, input().split())
+nums = list(map(int, input().split()))
+
+if action == 1:
+    print(7)
+elif action == 2:
+    if nums[0] > nums[1]:
+        print("Bigger")
+    elif nums[0] == nums[1]:
+        print("Equal")
+    else:
+        print("Smaller")
+elif action == 3:
+    print(sum(nums[:3]) - max(nums[:3]) - min(nums[:3]))
+elif action == 4:
+    print(sum(nums))
+elif action == 5:
+    print(sum(filter(lambda x: x % 2 == 0, nums)))
+elif action == 6:
+    print("".join(map(lambda x: ascii_lowercase[int(x % 26)], nums)))
+elif action == 7:
+    indices = set()
+    result = ["Done", "Cyclic", "Out"]
+    ind = 0
+    while True:
+        indices.add(ind)
+        ind = nums[ind]
+        check = [ind == num - 1, ind in indices, ind < 0 or ind >= num]
+        if any(check):
+            break
+
+    print("".join(map(lambda x, y: x * int(y), result, check)))
---
+++ B/Basic Programming 2.py
@@ -0,0 +1,36 @@
+"""Basic Programming 2"""
+
+from collections import Counter
+
+num, action = map(int, input().split())
+nums = list(map(int, input().split()))
+
+if action == 1:
+    nums = set(nums)
+    for i in nums:
+        if 7777 - i in nums:
+            print("Yes")
+            break
+    else:
+        print("No")
+elif action == 2:
+    if len(set(nums)) == len(nums):
+        print("Unique")
+    else:
+        print("Contains duplicate")
+elif action == 3:
+    nums = Counter(nums)
+    for k, v in nums.items():
+        if v > num / 2:
+            print(k)
+            break
+    else:
+        print(-1)
+elif action == 4:
+    nums.sort()
+    if num % 2:
+        print(nums[int(num / 2)])
+    else:
+        print(nums[int(num / 2) - 1], nums[int(num / 2)])
+elif action == 5:
+    print(*sorted(list(filter(lambda x: 100 <= x <= 999, nums))))
