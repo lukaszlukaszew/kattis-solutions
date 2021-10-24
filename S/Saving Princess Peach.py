6dcf36971802b437f8389f60b4507ccddb549361
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-24 09:22:34 +0200

    Add Saving for Carrots, Saving Princess Peach

---
+++ S/Saving Princess Peach.py
@@ -0,0 +1,16 @@
+"""Saving Princess Peach"""
+
+obstacles_count, mario = [int(x) for x in input().split()]
+
+obstacles = set(range(obstacles_count))
+mario_obstacles = set()
+
+for i in range(mario):
+    mario_obstacles.add(int(input()))
+
+obstacles = sorted(list(obstacles.difference(mario_obstacles)))
+
+for i in obstacles:
+    print(i)
+
+print('Mario got', len(mario_obstacles), 'of the dangerous obstacles.')
---
+++ S/Solving for Carrots.py
@@ -0,0 +1,8 @@
+"""Solving for Carrots"""
+
+N, P = map(int, input().split())
+
+for i in range(N):
+    input()
+
+print(P)
