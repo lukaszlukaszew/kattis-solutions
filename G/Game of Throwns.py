e852e8e02032f37a23bdddbb319d8f603e8f8d94
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-10-24 09:19:37 +0200

    Add Game of Throwns, Greedy Polygons

---
+++ G/Game of Throwns.py
@@ -0,0 +1,26 @@
+"""Game of Throwns"""
+
+students, moves_count = [int(x) for x in input().split()]
+moves = [
+    0,
+]
+student = 0
+
+commands = input().split()[::-1]
+
+while commands:
+    move = commands.pop()
+
+    if move == "undo":
+        move = int(commands.pop())
+
+        for i in range(move):
+            moves.pop()
+
+        student = moves[-1]
+        continue
+
+    student = int((student + int(move)) % students)
+    moves.append(student)
+
+print(student)
---
+++ G/Greedy Polygons.py
@@ -0,0 +1,13 @@
+"""Greedy Polygons"""
+
+import math
+
+cases = int(input())
+
+for i in range(cases):
+    n, l, d, g = map(int, input().split())
+
+    area = n * l * d * g + (d * g) ** 2 * math.pi
+    area += (n / 4) * (l ** 2) * (1 / (math.tan(math.radians(180 / n))))
+
+    print(area)
