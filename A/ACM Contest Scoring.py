5e22106eb3cd3fd546630dd2ba309e7f2fbacf53
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-01 08:38:49 +0100

    Add ACM Contest Scoring, ASCII Figure Rotation

---
+++ A/ACM Contest Scoring.py
@@ -0,0 +1,19 @@
+"""ACM Contest Scoring"""
+
+penalties = {}
+solved = 0
+points = 0
+
+while True:
+    try:
+        m, n, r = input().split()
+
+        if r == "wrong":
+            penalties[n] = penalties.get(n, 0) + 1
+        else:
+            points += int(m) + 20 * penalties.get(n, 0)
+            solved += 1
+
+    except ValueError:
+        print(solved, points)
+        break
---
+++ A/ASCII Figure Rotation.py
@@ -0,0 +1,34 @@
+"""ASCII Figure Rotation"""
+
+counter = 0
+
+while True:
+    lines = int(input())
+
+    if not lines:
+        break
+
+    if counter:
+        print()
+
+    shape, max_len = [], 0
+
+    for i in range(lines):
+        line = input()
+        max_len = max(max_len, len(line))
+        line = line.replace("|", "x")
+        line = line.replace("-", "|")
+        line = line.replace("x", "-")
+        shape.append(line)
+
+    for i in range(max_len):
+        line = ""
+        for j in range(lines - 1, -1, -1):
+            try:
+                line += shape[j][i]
+            except IndexError:
+                line += " "
+
+        print(line.rstrip())
+
+    counter += 1
