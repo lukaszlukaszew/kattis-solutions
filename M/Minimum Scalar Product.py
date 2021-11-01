942a9fe6c7a2d86e3b77c828c844178b32039f0e
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-01 08:40:48 +0100

    Add Minimum Scalar Product, Mirror Image

---
+++ M/Minimum Scalar Product.py
@@ -0,0 +1,15 @@
+"""Minimum Scalar Product"""
+
+cases = int(input())
+
+for i in range(cases):
+    coordinates = int(input())
+    result = 0
+
+    X = sorted([int(x) for x in input().split()])
+    Y = sorted([int(x) for x in input().split()], reverse=True)
+
+    for x, val in enumerate(X):
+        result += val * Y[x]
+
+    print('Case #' + str(i+1) + ':', result)
---
+++ M/Mirror Images.py
@@ -0,0 +1,15 @@
+"""Mirror Images"""
+
+cases = int(input())
+
+for i in range(cases):
+    R, C = map(int, input().split())
+    image = []
+
+    for j in range(R):
+        image.append(input())
+
+    print("Test", i + 1)
+
+    for j in image[::-1]:
+        print(j[::-1])
