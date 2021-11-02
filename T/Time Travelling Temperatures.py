895d83da4209b320dd51235e5bd4f08845705e95
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-02 07:07:11 +0100

    Add Time Travelling Temperatures, Tower Construction

---
+++ T/Time Travelling Temperatures.py
@@ -0,0 +1,10 @@
+"""Time Traveling Temperatures"""
+
+X, Y = [int(x) for x in input().split()]
+
+if Y == 1 and not X:
+    print("ALL GOOD")
+elif Y == 1:
+    print("IMPOSSIBLE")
+else:
+    print(X / (1 - Y))
---
+++ T/Tower Construction.py
@@ -0,0 +1,12 @@
+"""Tower Construction"""
+
+brick_count = int(input())
+bricks = [int(x) for x in input().split()]
+
+counter = 1
+
+for i in range(1, brick_count):
+    if bricks[i - 1] < bricks[i]:
+        counter += 1
+
+print(counter)
