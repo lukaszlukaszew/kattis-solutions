fd26140a3888e6c27d196f3b1a1b02f3574365cc
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-03 10:14:12 +0100

    Add R2, Ragged Right

---
+++ R/R2.py
@@ -0,0 +1,4 @@
+"""R2"""
+
+R1, S = map(int, input().split())
+print(2 * S - R1)
---
+++ R/Ragged Right.py
@@ -0,0 +1,17 @@
+"""Ragged Right"""
+
+penalty, max_len = 0, 0
+lens = []
+
+try:
+    while True:
+        line = input()
+        lens.append(len(line))
+except EOFError:
+    max_len = max(lens)
+    lens.pop()
+
+while lens:
+    penalty += (max_len - lens.pop()) ** 2
+
+print(penalty)
