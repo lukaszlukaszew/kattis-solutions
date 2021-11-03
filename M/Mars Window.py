e6a914cf45186f12ef40062bf93493d2c059ad42
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-03 09:49:58 +0100

    Add Mars Window, Mastering Mastermind

---
+++ M/Mars Window.py
@@ -0,0 +1,13 @@
+"""Mars Window"""
+
+year = int(input())
+
+result = 0
+
+for i in range(year * 12 + 1, (year + 1) * 12 + 1):
+    result = max(result, int((i - 2018 * 12 - 4) % 26 == 0))
+
+if result:
+    print("yes")
+else:
+    print("no")
---
+++ M/Mastering Mastermind.py
@@ -0,0 +1,17 @@
+"""Mastering Mastermind"""
+
+n, first, second = input().split()
+first, second = list(first), list(second)
+r, s = 0, 0
+
+for i, val in enumerate(first):
+    if val == second[i]:
+        first[i], second[i] = "", ""
+        r += 1
+
+for i, val in enumerate(first):
+    if val and val in second:
+        second[second.index(val)], first[i] = "", ""
+        s += 1
+
+print(r, s)
