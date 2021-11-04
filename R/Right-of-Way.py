f27998d337317472715a9bdc49af884fd6737738
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-04 07:46:11 +0100

    Add Right-of-Way, Rijeci

---
+++ R/Right-of-Way.py
@@ -0,0 +1,16 @@
+"""Right-of-Way"""
+
+result = "NO"
+
+right = {"South": "East", "East": "North", "North": "West", "West": "South"}
+left = {"South": "West", "East": "South", "North": "East", "West": "North"}
+front = {"South": "North", "East": "West", "North": "South", "West": "East"}
+
+start, end, another = input().split()
+
+if (another == right[start] and end != right[start]) or (
+    another == front[start] and end == left[start]
+):
+    result = "YES"
+
+print(result)
---
+++ R/Riječi.py
@@ -0,0 +1,9 @@
+"""Riječi"""
+
+K = int(input())
+A, B = 1, 0
+
+for i in range(K):
+    A, B = B, A + B
+
+print(A, B)
