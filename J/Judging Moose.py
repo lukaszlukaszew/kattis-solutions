fdb2b49d390152d2baf5f2bf8c737a258be34e42
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-05 07:05:23 +0100

    Add Judging Moose, Judging Troubles

---
+++ J/Judging Moose.py
@@ -0,0 +1,11 @@
+"""Judging Moose"""
+
+L, R = map(int, input().split())
+
+if L == R:
+    if L:
+        print("Even", 2 * L)
+    else:
+        print("Not a moose")
+else:
+    print("Odd", max(L, R) * 2)
---
+++ J/Judging Troubles.py
@@ -0,0 +1,20 @@
+"""Judging Troubles"""
+
+from sys import stdin
+from collections import defaultdict
+
+submissions = int(stdin.readline())
+
+DOMjudge, KATTISjudge = defaultdict(int), defaultdict(int)
+result = 0
+
+for i in range(submissions):
+    DOMjudge[stdin.readline()] += 1
+
+for i in range(submissions):
+    KATTISjudge[stdin.readline()] += 1
+
+for k, v in DOMjudge.items():
+    result += min(v, KATTISjudge[k])
+
+print(result)
