2f98b6fd4d6693b2826e72309d82e97886d8e867
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-07 08:14:41 +0100

    Add Mjehuric, Moving Day

---
+++ M/Mjehuric.py
@@ -0,0 +1,16 @@
+"""Mjehuric"""
+
+import copy
+
+nums = input().split()
+nums_copy = copy.deepcopy(nums)
+nums_copy.sort()
+
+while True:
+    for i in range(4):
+        if nums[i] >= nums[i + 1]:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            print(*nums)
+
+    if nums_copy == nums:
+        break
---
+++ M/Moving Day.py
@@ -0,0 +1,12 @@
+"""Moving Day"""
+
+boxes, V = [int(x) for x in input().split()]
+
+for i in range(boxes):
+    x, y, z = [int(x) for x in input().split()]
+    if not i:
+        result = x * y * z - V
+
+    result = max(result, x * y * z - V)
+
+print(result)
