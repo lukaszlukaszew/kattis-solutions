ad035efbdd6bd173315e199be2f07a64e8bb317b
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-07 08:15:51 +0100

    Add NOP, Number Fun

---
+++ N/Number Fun.py
@@ -0,0 +1,11 @@
+"""Number Fun"""
+
+cases = int(input())
+
+for i in range(cases):
+    a, b, c = map(int, input().split())
+
+    if c in [a + b, a * b, abs(a - b), a / b, b / a]:
+        print("Possible")
+    else:
+        print("Impossible")
