f512c4756b45b64c74e34c3e331be08a81bf30de
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-08 08:17:01 +0100

    Add Password Hacking, Prva

---
+++ P/Password Hacking.py
@@ -0,0 +1,14 @@
+"""Password Hacking"""
+
+count = int(input())
+probabilities, result = [], 0
+
+for i in range(count):
+    probabilities.append(float(input().split()[1]))
+
+probabilities.sord(reverse=True)
+
+for i, val in enumerate(probabilities):
+    result += val * (i + 1)
+
+print(result)
