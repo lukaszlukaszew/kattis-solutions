6f997dfe2e1553893fe70b3d950b4f2857dd25fc
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-08 08:16:18 +0100

    Add Jumbled Compass, Just a Minute

---
+++ J/Jumbled Compass.py
@@ -0,0 +1,15 @@
+"""Jumbled Compass"""
+
+
+def change(x, y):
+    """Comparision function"""
+    clockwise = y - x + 360 * int(x > y)
+    cclockiwse = -(360 - clockwise)
+
+    if abs(clockwise) <= abs(cclockiwse):
+        return clockwise
+
+    return cclockiwse
+
+
+print(change(int(input()), int(input())))
---
+++ J/Just a Minute.py
@@ -0,0 +1,17 @@
+"""Just a Minute"""
+
+observations = int(input())
+minutes, seconds = [], []
+
+for i in range(observations):
+    m, s = map(int, input().split())
+
+    minutes.append(m)
+    seconds.append(s)
+
+avg = sum(seconds) / sum(minutes) / 60
+
+if avg <= 1:
+    print("measurement error")
+else:
+    print(avg)
