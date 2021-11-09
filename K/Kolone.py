3997a2e17b6caca2c55d4bc474759c0e5b71eaea
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-09 07:55:02 +0100

    Add Kolone, Krizaljka

---
+++ K/Kolone.py
@@ -0,0 +1,14 @@
+"""Kolone"""
+
+lt, rt = [int(x) for x in input().split()]
+
+left = list(input())[::-1]
+right = list(input())
+
+time = int(input())
+
+while left:
+    right.insert(min(rt, time), left.pop())
+    time = max(time - 1, 0)
+
+print("".join(right))
---
+++ K/Križaljka.py
@@ -0,0 +1,15 @@
+"""Križaljka"""
+
+horizontal, vertical = input().split()
+
+for i in horizontal:
+    if i in vertical:
+        h = horizontal.index(i)
+        v = vertical.index(i)
+        break
+
+for i, val in enumerate(vertical):
+    if i == v:
+        print(horizontal)
+    else:
+        print("." * h + val + (len(horizontal) - h - 1) * ".")
