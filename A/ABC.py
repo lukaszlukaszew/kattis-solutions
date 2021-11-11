044dbe55dad875a04783dd22254448b2fd9f4f4c
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-11 08:12:41 +0100

    Add ABC, Coloring Socks

---
+++ A/ABC.py
@@ -0,0 +1,6 @@
+"""ABC"""
+
+nums = sorted(map(int, input().split()))
+line = input()
+
+print(" ".join([str(nums[ord(x) - ord("A")]) for x in line]))
---
+++ C/Coloring Socks.py
@@ -0,0 +1,18 @@
+"""Coloring Socks"""
+
+from sys import stdin
+
+S, C, K = [int(x) for x in stdin.readline().split()]
+socks = sorted(map(int, stdin.readline().split()))
+
+machines, current, minimum = 1, 0, socks[0]
+
+for i in socks:
+    if abs(minimum - i) <= K and current < C:
+        current += 1
+    else:
+        minimum = i
+        machines += 1
+        current = 1
+
+print(machines)
