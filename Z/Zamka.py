c594f4749d93ed9193ae846a4a4361573296dfbe
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-11 08:11:18 +0100

    Add Zamka, Zyxab

---
+++ Z/Zamka.py
@@ -0,0 +1,15 @@
+"""Zamka"""
+
+minimum = int(input())
+maximum = int(input())
+summary = int(input())
+
+for j in range(minimum, maximum + 1):
+    if sum(map(int, list(str(j)))) == summary:
+        print(j)
+        break
+
+for j in range(maximum, minimum - 1, -1):
+    if sum(map(int, list(str(j)))) == summary:
+        print(j)
+        break
---
+++ Z/Zyxab.py
@@ -0,0 +1,18 @@
+"""Zyxab"""
+
+from sys import maxsize
+
+lines = int(input())
+current_len, current_word = maxsize, "neibb!"
+
+for i in range(lines):
+    word = input()
+    letters = list(word)
+
+    if not (len(letters) < 5 or len(letters) != len(set(letters))):
+        if len(letters) < current_len:
+            current_word, current_len = word, len(letters)
+        elif len(letters) == current_len:
+            current_word = max(word, current_word)
+
+print(current_word)
