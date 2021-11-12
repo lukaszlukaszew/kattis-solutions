8669b7265e27fdd606c7f365bfaed6457439a163
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-12 08:08:51 +0100

    Add Billiard, Bishops

---
+++ B/Billiard.py
@@ -0,0 +1,17 @@
+"""Billiard"""
+
+from math import sqrt, degrees, acos
+
+line = [int(x) for x in input().split()]
+
+while sum(line):
+    a, b, s, m, n = line
+
+    x, y = a * m, b * n
+    distance = sqrt(x ** 2 + y ** 2)
+    v = distance / s
+    angle = degrees(acos(x / distance))
+
+    print(f"{angle:.2f} {v:.2f}")
+
+    line = [int(x) for x in input().split()]
---
+++ B/Bishops.py
@@ -0,0 +1,10 @@
+"""Bishops"""
+
+import sys
+
+try:
+    while True:
+        num = int(input())
+        print(max(2 * (num - 1), num))
+except EOFError:
+    sys.exit()
