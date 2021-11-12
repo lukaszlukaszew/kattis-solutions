6342d39872d5fd915f85f844fdec2d38988da629
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-12 08:10:39 +0100

    Add Flexible Spaces, Functional Fun

---
+++ F/Flexible Spaces.py
@@ -0,0 +1,12 @@
+"""Flexible Spaces"""
+
+from itertools import combinations
+
+W, P = map(int, input().split())
+spaces = [*list(map(int, input().split())), 0, W]
+result = set()
+for i in combinations(spaces, 2):
+    result.add(abs(i[0] - i[1]))
+
+result.discard(0)
+print(*sorted(set(result)))
---
+++ F/Functional Fun.py
@@ -0,0 +1,31 @@
+"""Functional Fun"""
+
+import sys
+
+while True:
+    try:
+        domain = set(sys.stdin.readline().split()[1:])
+        codomain = set(sys.stdin.readline().split()[1:])
+        cases = int(sys.stdin.readline())
+        arguments, values = [], []
+
+        for i in range(cases):
+            x, y = sys.stdin.readline().rstrip().split(" -> ")
+            arguments.append(x)
+            values.append(y)
+
+        if len(arguments) != len(set(arguments)):
+            result = "not a function"
+        elif set(values) == codomain and len(set(arguments)) == len(codomain):
+            result = "bijective"
+        elif set(values) == codomain and len(set(values)) < len(values):
+            result = "surjective"
+        elif len(set(values)) == len(values) and len(values) == len(arguments):
+            result = "injective"
+        else:
+            result = "neither injective nor surjective"
+
+        print(result)
+
+    except ValueError:
+        sys.exit()
