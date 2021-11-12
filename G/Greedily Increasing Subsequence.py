a14713ac10e7df6f38b43499159f056e5d144764
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-12 08:12:00 +0100

    Add Gerrymandering, Greedily Increasing Subsequence

---
+++ G/Greedily Increasing Subsequence.py
@@ -0,0 +1,15 @@
+"""Greedily Increasing Subsequence"""
+
+from sys import stdin
+
+stdin.readline()
+nums = [int(x) for x in stdin.readline().split()]
+current, result = 0, []
+
+for i in nums:
+    if i > current:
+        result.append(i)
+        current = i
+
+print(len(result))
+print(*result)
