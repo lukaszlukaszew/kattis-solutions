53361e54235f1aeeb77c2d51dacb93369e98de5e
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-13 09:32:20 +0100

    Add Veci, Verify This, Your Majesty

---
+++ V/Veci.py
@@ -0,0 +1,16 @@
+"""Veci"""
+
+from itertools import permutations
+
+number = list(input())
+
+perm = list(
+    filter(
+        lambda x: x > int("".join(number)),
+        map(lambda x: int("".join(x)), (permutations(number, len(number)))),
+    )
+)
+if perm:
+    print(min(perm))
+else:
+    print(0)
---
+++ V/Verify This, Your Majesty.py
@@ -0,0 +1,21 @@
+"""Verify This, Your Majesty"""
+
+from sys import stdin
+
+size = int(stdin.readline())
+rows, columns, one, two = set(), set(), set(), set()
+
+for i in range(size):
+    x, y = [int(x) for x in stdin.readline().split()]
+    rows.add(y)
+    columns.add(x)
+    one.add(x - y)
+    two.add(7 - x - y)
+
+result = {len(rows), len(columns), len(one), len(two)}
+result.discard(size)
+
+if result:
+    print("INCORRECT")
+else:
+    print("CORRECT")
