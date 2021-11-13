d6ad1c53064860ffdefe1820be4895945bc877fd
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-13 09:33:49 +0100

    Add Heart Rate, Okviri

---
+++ H/Heart Rate.py
@@ -0,0 +1,11 @@
+"""Heart Rate"""
+
+cases = int(input())
+
+for i in range(cases):
+    b, p = map(float, input().split())
+    print(
+        format(60 * b / p - 60 / p, ".4f"),
+        format(60 * b / p, ".4f"),
+        format(60 * b / p + 60 / p, ".4f"),
+    )
