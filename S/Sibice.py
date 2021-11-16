cb86c914035e5b8cb20b03cf155f56c6796ad1cd
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-16 07:22:30 +0100

    Add Sibice, Simplicity

---
+++ S/Sibice.py
@@ -0,0 +1,12 @@
+"""Sibice"""
+
+from math import sqrt
+
+N, W, H = map(int, input().split())
+
+for j in range(N):
+    length = int(input())
+    if length <= sqrt(W ** 2 + H ** 2):
+        print("DA")
+    else:
+        print("NE")
---
+++ S/Simplicity.py
@@ -0,0 +1,15 @@
+"""Simplicity"""
+
+from collections import Counter
+
+word = Counter(input())
+simplicity = 0
+
+while len(word) > 2:
+    for k, v in word.items():
+        if v == min(word.values()):
+            simplicity += v
+            word.pop(k)
+            break
+
+print(simplicity)
