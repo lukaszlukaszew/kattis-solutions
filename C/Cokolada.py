f8af4a8ea4dbb762dedd1aaa626f203d51bc39fa
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-17 07:22:04 +0100

    Add Cokolada, Conformity

---
+++ C/Cokolada.py
@@ -0,0 +1,17 @@
+"""Cokolada"""
+
+pieces = int(input())
+choc_bar, breaks = 1, 0
+
+while pieces > choc_bar:
+    choc_bar *= 2
+
+max_bar = choc_bar
+
+while pieces and pieces != choc_bar:
+    choc_bar = int(choc_bar / 2)
+    if pieces > choc_bar:
+        pieces -= choc_bar
+    breaks += 1
+
+print(max_bar, breaks)
