18efd6b87956547a397aaf427a369bb5c51e73da
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-18 07:47:43 +0100

    Add Deathstar, Delimiter Soup

---
+++ D/Delimiter Soup.py
@@ -0,0 +1,18 @@
+"""Delimiter Soup"""
+
+input()
+delimiters = {"}": "{", "]": "[", ")": "("}
+line = input()
+check = []
+
+for i, val in enumerate(line):
+    if val in "({[":
+        check.append(val)
+    elif val in ")}]":
+        if check and check[-1] == delimiters[val]:
+            check.pop()
+        else:
+            print(val, i)
+            break
+else:
+    print("ok so far")
