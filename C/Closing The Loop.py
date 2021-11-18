e942dd27a56e3ab95e7d3ceb8f8c58e2d5e6a5d0
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-18 07:47:07 +0100

    Add Closing The Loop, Coconut Splat

---
+++ C/Closing The Loop.py
@@ -0,0 +1,21 @@
+"""Closing The Loop"""
+
+from collections import defaultdict
+
+cases = int(input())
+
+for i in range(cases):
+    input()
+    line = input().split()
+    segments, result = defaultdict(list), 0
+
+    for j in line:
+        segments[j[-1]].append(int(j[:-1]))
+
+    for k, v in segments.items():
+        v.sort()
+
+    while segments["B"] and segments["R"]:
+        result += segments["B"].pop() + segments["R"].pop() - 2
+
+    print(f'Case #{i+1}: {result}')
---
+++ C/Coconut Splat.py
@@ -0,0 +1,23 @@
+"""Coconut Splat"""
+
+syllables, players_count = [int(x) for x in input().split()]
+fists, ind = [], 0
+
+for i in range(players_count):
+    fists.append(str(i + 1) + "F")
+
+while len(fists) > 1:
+    ind = int((ind + syllables - 1) % len(fists))
+    state = fists[ind][-1]
+    player = fists[ind][:-1]
+
+    del fists[ind]
+
+    if state == "F":
+        fists.insert(ind, player + "H")
+        fists.insert(ind, player + "H")
+    elif state == "H":
+        fists.insert(ind, player + "P")
+        ind += 1
+
+print(int(fists[0][:-1]))
