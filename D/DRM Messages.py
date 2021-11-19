c21b100f3a99eff02fcd70fe693fcad2d0bf2b44
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-19 07:02:34 +0100

    Add DRM Messages, Drinking Song

---
+++ D/DRM Messages.py
@@ -0,0 +1,28 @@
+"""DRM Messages"""
+
+from string import ascii_uppercase
+
+drm = input()
+letters, result = ascii_uppercase, ""
+drms, shifts, new_drms = {}, {}, {}
+
+for i in range(2):
+    if i:
+        drms[i] = drm[int(len(drm) / 2):]
+    else:
+        drms[i] = drm[: int(len(drm) / 2)]
+
+    for j in drms[i]:
+        shifts[i] = shifts.get(i, 0) + letters.index(j)
+
+    for j in drms[i]:
+        new_drms[i] = (
+            new_drms.get(i, "") + letters[(letters.index(j) + shifts[i]) % len(letters)]
+        )
+
+for i, val in enumerate(new_drms[0]):
+    result += letters[
+        (letters.index(val) + letters.index(new_drms[1][i])) % len(letters)
+    ]
+
+print(result)
---
+++ D/Drinking Song.py
@@ -0,0 +1,16 @@
+"""Drinking Song"""
+
+a = int(input())
+b = input()
+
+while a:
+    e1 = "s" * (a > 1)
+    e2 = "s" * (a > 2) + "s" * (a == 1)
+    e3 = " on the wall" * (a > 1)
+    l1 = "it" * (a == 1) + "one" * (a > 1)
+    l2 = "no more" * (a == 1) + f"{a-1}" * (a > 1)
+
+    print(f"{a} bottle{e1} of {b} on the wall, {a} bottle{e1} of {b}.")
+    print(f"Take {l1} down, pass it around, {l2} bottle{e2} of {b}{e3}.\n")
+
+    a -= 1
