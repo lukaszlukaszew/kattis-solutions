5e211a828ed388569b33c6c3f5a969e413457ccf
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-21 15:11:24 +0100

    Add Pairing Socks, Problem Classification

---
+++ P/Pairing Socks.py
@@ -0,0 +1,25 @@
+"""Pairing Socks"""
+
+from sys import stdin
+
+socks = int(stdin.readline())
+left = stdin.readline().rstrip().split()[::-1]
+right, moves = [], 0
+
+while left:
+    if right:
+        if left[-1] == right[-1]:
+            right.pop()
+            left.pop()
+        else:
+            right.append(left.pop())
+
+    else:
+        right.append(left.pop())
+
+    moves += 1
+
+if right:
+    print("impossible")
+else:
+    print(moves)
---
+++ P/Problem Classification.py
@@ -0,0 +1,26 @@
+"""Problem Classification"""
+
+from sys import stdin
+from collections import Counter
+
+keywords, result = {}, {}
+categories = int(stdin.readline())
+
+for i in range(categories):
+    keyword, *words = stdin.readline().split()
+    keywords[keyword] = set(words)
+
+text = Counter()
+line = stdin.readline().split()
+
+while line:
+    text.update(line)
+    line = stdin.readline().split()
+
+for k, v in keywords.items():
+    result[k] = sum(map(lambda x: text[x], v))
+
+keys = sorted(filter(lambda x: result[x] == max(result.values()), result.keys()))
+
+for key in keys:
+    print(key)
