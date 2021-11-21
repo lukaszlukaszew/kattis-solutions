4009799f04544b8adb9c64c15b0fedc9cbf00074
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-21 15:10:32 +0100

    Add A List Game, A Towering Problem

---
+++ A/A List Game.py
@@ -0,0 +1,37 @@
+"""A List Game"""
+
+from math import sqrt
+
+X = int(input())
+points = 0
+
+
+def primes(limit):
+    """Primes generator"""
+    primes_list = []
+
+    if primes_list:
+        z = max(primes_list)
+    else:
+        z = 1
+
+    for k in range(z + 1, limit + 1):
+        for j in primes_list:
+            if k % j == 0:
+                break
+        else:
+            yield k
+            primes_list.append(k)
+
+    return primes_list
+
+
+for i in primes(int(sqrt(X)) + 1):
+    while X % i == 0:
+        points += 1
+        X /= i
+
+if X > int(sqrt(X)) + 1:
+    points += 1
+
+print(points)
---
+++ A/A Towering Problem.py
@@ -0,0 +1,16 @@
+"""A Towering Problem"""
+
+from itertools import combinations
+
+*boxes, h1, h2 = map(int, input().split())
+groups, group1, group2 = [], [], []
+
+for i in combinations(boxes, 3):
+    if sum(i) == h1 and not group1:
+        group1 = i
+    elif sum(i) == h2 and not group2:
+        group2 = i
+    elif group1 and group2:
+        break
+
+print(*sorted(group1, reverse=True), *sorted(group2, reverse=True))
