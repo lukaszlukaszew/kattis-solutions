1b3dd0a2cdfbac590a3ecf945191b0fa8eda6fff
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-23 07:29:57 +0100

    Add Temperature Confusion, Thanos

---
+++ T/Temperature Confusion.py
@@ -0,0 +1,25 @@
+"""Temperature Confusion"""
+
+
+def nwd(a, b):
+    """greatest common divisor"""
+    while b:
+        a, b = b, a % b
+    return a
+
+
+fnumerator, fdenominator = map(int, input().split("/"))
+cnumerator, cdenominator = -32, 1
+
+if fdenominator != cdenominator:
+    fnumerator, fdenominator, cnumerator, cdenominator = (
+        fnumerator * cdenominator,
+        fdenominator * cdenominator,
+        cnumerator * fdenominator,
+        cdenominator * fdenominator,
+    )
+
+cnumerator, cdenominator = (fnumerator + cnumerator) * 5, cdenominator * 9
+cnwd = nwd(cnumerator, cdenominator)
+
+print(str(int(cnumerator / cnwd)) + "/" + str(int(cdenominator / cnwd)))
---
+++ T/Thanos.py
@@ -0,0 +1,12 @@
+"""Thanos"""
+
+cases = int(input())
+
+for i in range(cases):
+    P, R, F = [int(x) for x in input().split()]
+    years = 0
+    while P <= F:
+        P *= R
+        years += 1
+
+    print(years)
