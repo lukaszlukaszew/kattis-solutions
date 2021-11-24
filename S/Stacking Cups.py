fe1f4ffdce6e28b78b171863a048917fb26b5884
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-24 08:10:57 +0100

    Add Stacking Cups, Sok

---
+++ S/Stacking Cups.py
@@ -0,0 +1,17 @@
+"""Stacking Cups"""
+
+cups = int(input())
+result = []
+
+for i in range(cups):
+    line = input().split()
+
+    if line[0].isnumeric():
+        result.append((int(line[0]) / 2, line[1]))
+    else:
+        result.append((int(line[1]), line[0]))
+
+result.sort()
+
+for i in result:
+    print(i[1])
