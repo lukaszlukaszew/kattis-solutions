b13ab05d2ea843a3096ca104afa2a17f2c374959
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-26 09:02:53 +0100

    Add Islands, Islands in the Data Stream

---
+++ I/Islands in the Data Stream.py
@@ -0,0 +1,26 @@
+"""Islands in the Data Stream"""
+
+
+def counter(num):
+    """Count islands"""
+    global c, stock
+
+    if num > stock[-1]:
+        stock.append(num)
+        c += 1
+    elif num < stock[-1]:
+        stock.pop()
+        c += 1
+        counter(num)
+
+
+cases = int(input())
+
+for i in range(cases):
+    c, stock = 0, [0]
+    stream = [int(x) for x in input().split()][2:]
+
+    for j in stream:
+        counter(j)
+
+    print(i + 1, int(c / 2))
---
+++ I/Islands.py
@@ -0,0 +1,26 @@
+"""Islands"""
+
+r, c = [int(x) for x in input().split()]
+picture, islands = [], 0
+
+
+def change(x, y, pic):
+    pic[y][x] = "W"
+
+    for m in (-1, 0, 1):
+        for n in (-1, 0, 1):
+            if 0 <= x + m < c and 0 <= y + n < r and abs(m) != abs(n):
+                if pic[y + n][x + m] in "LC":
+                    change(x + m, y + n, pic)
+
+
+for i in range(r):
+    picture.append(list(input()))
+
+for i in range(r):
+    for j in range(c):
+        if picture[i][j] == "L":
+            islands += 1
+            change(j, i, picture)
+
+print(islands)
