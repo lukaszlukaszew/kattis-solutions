c44dd3f2bd64b082d7f98bfbc13c99655fd8e872
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-28 07:38:22 +0100

    Add Height Ordering, Hot Springs

---
+++ H/Height Ordering.py
@@ -0,0 +1,20 @@
+"""Height Ordering"""
+
+cases = int(input())
+
+for i in range(cases):
+    case, *heights = map(int, input().split())
+    ordered, steps = [], 0
+
+    ordered.append(heights.pop(0))
+
+    for j in heights:
+        for k, val in enumerate(ordered):
+            if j > val and k == len(ordered) - 1:
+                ordered.append(j)
+            elif j < val:
+                steps += len(ordered[k:])
+                ordered.insert(k, j)
+                break
+
+    print(case, steps)
---
+++ H/Hot Springs.py
@@ -0,0 +1,16 @@
+"""Hot Springs"""
+
+from sys import stdin
+
+nums = int(stdin.readline())
+num = sorted(map(int, stdin.readline().split()))
+
+left = num[: int(nums / 2)]
+right = sorted(num[int(nums / 2):], reverse=True)
+result = []
+
+while left and right:
+    result.append(right.pop())
+    result.append(left.pop())
+
+print(*result, *left, *right)
