6e176e51baa2b745bca92831cd7065714889376a
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-28 07:37:41 +0100

    Add I Wanna Be The Very Best, iBoard

---
+++ I/I Wanna Be The Very Best.py
@@ -0,0 +1,20 @@
+"""I Wanna Be The Very Best"""
+
+pokes, K = [int(x) for x in input().split()]
+stats = {0: {}, 1: {}, 2: {}}
+
+for i in range(pokes):
+    line = [int(x) for x in input().split()]
+    for j, val in enumerate(line):
+        stats[j][val] = i
+
+result = set()
+
+for i in range(3):
+    for j, key in enumerate(sorted(stats[i].keys(), reverse=True)):
+        if j < K:
+            result.add(stats[i][key])
+        else:
+            break
+
+print(len(result))
