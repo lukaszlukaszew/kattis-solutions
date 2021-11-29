188a0e521c1031776a257b191fd071238ccdedd7
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-29 07:52:56 +0100

    Add Get to Work, Guess the Number

---
+++ G/Get to Work.py
@@ -0,0 +1,38 @@
+"""Get to Work"""
+
+from collections import defaultdict
+
+cases = int(input())
+
+for i in range(cases):
+    N, T = [int(x) for x in input().split()]
+    N = list(range(1, N + 1))
+
+    employees, capacities = defaultdict(int), defaultdict(list)
+    cars, empl = defaultdict(int), int(input())
+
+    for j in range(empl):
+        town, capacity = [int(x) for x in input().split()]
+        employees[town] += 1
+        capacities[town].append(capacity)
+
+    for k in N:
+        if k == T:
+            employees[k] = 0
+            continue
+
+        for j in sorted(capacities[k], reverse=True):
+            if employees[k] <= 0:
+                break
+
+            employees[k] -= min(j, employees[k])
+            cars[k] += 1
+
+    result = "Case #" + str(i + 1) + ":"
+
+    if sum(employees.values()):
+        caps = ["IMPOSSIBLE"]
+    else:
+        caps = [cars[x] for x in N]
+
+    print(result, *caps)
---
+++ G/Guess the Number.py
@@ -0,0 +1,20 @@
+"""Guess the Number"""
+
+import sys
+
+minimum, maximum, guess = 1, 1000, 500
+
+for i in range(10):
+    print(guess)
+    answer = input()
+
+    if answer != "correct":
+        if answer == "lower":
+            maximum = guess - 1
+        elif answer == "higher":
+            minimum = guess + 1
+
+        guess = int((minimum + maximum) / 2)
+
+    else:
+        sys.exit()
