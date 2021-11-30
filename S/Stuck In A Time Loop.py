e0b46b7af8592c8a4b325807235b343126c90f10
lukaszlukaszew <lukaszlukaszew@users.noreply.github.com>
2021-11-30 07:05:13 +0100

    Add Stuck In A Time Loop, Sum Squared Digits Function

---
+++ S/Stuck In A Time Loop.py
@@ -0,0 +1,4 @@
+"""Stuck In A Time Loop"""
+
+for i in range(int(input())):
+    print(i + 1, "Abracadabra")
---
+++ S/Sum Squared Digits Function.py
@@ -0,0 +1,21 @@
+"""Sum Squared Digits Function"""
+
+cases = int(input())
+
+for i in range(cases):
+    case, base, number = [int(x) for x in input().split()]
+    exponents, j, result = [], 0, 0
+    exponent = base ** j
+
+    while exponent <= number:
+        exponents.append(exponent)
+        j += 1
+        exponent = base ** j
+
+    exponents.sort(reverse=True)
+
+    for j in exponents:
+        result += int((number // j) ** 2)
+        number = int(number % j)
+
+    print(case, result)
